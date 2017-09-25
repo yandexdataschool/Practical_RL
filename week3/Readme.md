251c1345c48111187e51a201c518f70a18a90b76
Just Heuristic <justheuristic@gmail.com>
2017-09-25 19:41:53 +0300

    week3 seminar

--- week3/Readme.md
+++
@@ -1,9 +0,0 @@
-# Model-free reinforcement learning
-
-Current seminar and home assignment consists of 5 parts:
-
-0. QLearning
-1. Gym task 
-2. Experience Replay
-3. Binarization 
-4. EV-sarsa and research
--- week3/seminar_alternative/qlearning.py
+++ week3/seminar_alternative/qlearning.py
@@ -1,40 +1,31 @@
-"""
-Expected Value SARSA
-This file builds upon the same functions as Q-learning agent (qlearning.py).
+# qlearningAgents.py
+# ------------------
+## based on http://inst.eecs.berkeley.edu/~cs188/sp09/pacman.html
 
-[assignment]
-The only thing you must implement is the getValue method.
-- Recall that V(s) in SARSA is not the maximal but the expected Q-value.
-- The expectation should be done under agent's policy (e-greedy).
-
-
-Here's usage example:
->>>from sarsa import SarsaAgent
-
->>>agent = SarsaAgent(alpha=0.1,epsilon=0.25,discount=0.99,
-                       getLegalActions = lambda s: actions_from_that_state)
->>>action = agent.getAction(state)
->>>agent.update(state,action, next_state,reward)
->>>agent.epsilon *= 0.99
-"""
 import random,math
 
 import numpy as np
 from collections import defaultdict
 
-class SarsaAgent():
+class QLearningAgent():
   """
-    Classical SARSA agent.
-    
-    The two main methods are 
-    - self.getAction(state) - returns agent's action in that state
-    - self.update(state,action,reward,nextState,nextAction) - returns agent's next action
+    Q-Learning Agent
 
     Instance variables you have access to
       - self.epsilon (exploration prob)
       - self.alpha (learning rate)
       - self.discount (discount rate aka gamma)
 
+    Functions you should use
+      - self.getLegalActions(state)
+        which returns legal actions for a state
+      - self.getQValue(state,action)
+        which returns Q(state,action)
+      - self.setQValue(state,action,value)
+        which sets Q(state,action) := value
+    
+    !!!Important!!!
+    NOTE: please avoid using self._qValues directly to make code cleaner
   """
   def __init__(self,alpha,epsilon,discount,getLegalActions):
     "We initialize agent and Q-values here."
@@ -58,6 +49,20 @@ class SarsaAgent():
 
 #---------------------#start of your code#---------------------#
 
+  def getValue(self, state):
+    """
+      Returns max_action Q(state,action)
+      where the max is over legal actions.
+    """
+    
+    possibleActions = self.getLegalActions(state)
+    #If there are no legal actions, return 0.0
+    if len(possibleActions) == 0:
+    	return 0.0
+
+    "*** YOUR CODE HERE ***"
+    return <compute state value>
+    
   def getPolicy(self, state):
     """
       Compute the best action to take in a state. 
@@ -71,8 +76,8 @@ class SarsaAgent():
     
     best_action = None
 
-    "*** this code works exactly as Q-learning ***"
-    best_action = possibleActions[np.argmax([self.getQValue(state, a) for a in possibleActions])]
+    "*** YOUR CODE HERE ***"
+    best_action = <your code>
     return best_action
 
   def getAction(self, state):
@@ -98,14 +103,11 @@ class SarsaAgent():
     #agent parameters:
     epsilon = self.epsilon
 
-    "*** Epsilon-greedy strategy exactly as Q-learning ***"
-    if np.random.random()<=epsilon:
-    	return random.choice(possibleActions)
-    else:
-    	action = self.getPolicy(state)
-    return action
+    "*** YOUR CODE HERE ***"
+    
+    return <put agent's action here>
 
-  def update(self, state, action, nextState,nextAction, reward):
+  def update(self, state, action, nextState, reward):
     """
       You should do your Q-Value update here
 
@@ -119,10 +121,9 @@ class SarsaAgent():
     learning_rate = self.alpha
     
     "*** YOUR CODE HERE ***"    
-    reference_qvalue = <Your Code Here>
+    reference_qvalue = <the "correct state value", uses reward and the value of next state>
     
     updated_qvalue = (1-learning_rate) * self.getQValue(state,action) + learning_rate * reference_qvalue
-    
     self.setQValue(state,action,updated_qvalue)
 
 
