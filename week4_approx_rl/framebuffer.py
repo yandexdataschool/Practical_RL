a5c13125948a96e958d4def62c72bdfe36df25b5
peter <piter.zh@gmail.com>
2018-02-22 13:08:17 +0300

    Fixed framebuffer for PyTorch homework. Fixed tests for the Preprocessing Atari class. Added more understandable "Here is what the network gonna see" picture (with a ball).

--- week4_approx_rl/framebuffer.py
+++ week4_approx_rl/framebuffer.py
@@ -6,9 +6,14 @@ class FrameBuffer(Wrapper):
         """A gym wrapper that reshapes, crops and scales image into the desired shapes"""
         super(FrameBuffer, self).__init__(env)
         self.dim_order = dim_order
-        height, width, n_channels = env.observation_space.shape
-        obs_shape = [height, width, n_channels * n_frames] if dim_order == 'tensorflow' \
-                     else [n_channels * n_frames, height, width]
+        if dim_order == 'tensorflow':
+            height, width, n_channels = env.observation_space.shape
+            obs_shape = [height, width, n_channels * n_frames]
+        elif dim_order == 'pytorch':
+            n_channels, height, width = env.observation_space.shape
+            obs_shape = [n_channels * n_frames, height, width]
+        else:
+            raise ValueError('dim_order should be "tensorflow" or "pytorch", got {}'.format(dim_order))
         self.observation_space = Box(0.0, 1.0, obs_shape)
         self.framebuffer = np.zeros(obs_shape, 'float32')
         
@@ -25,12 +30,12 @@ class FrameBuffer(Wrapper):
         return self.framebuffer, reward, done, info
     
     def update_buffer(self, img):
-        offset = self.env.observation_space.shape[-1]
         if self.dim_order == 'tensorflow':
+            offset = self.env.observation_space.shape[-1]
             axis = -1
             cropped_framebuffer = self.framebuffer[:,:,:-offset]
-        else:
+        elif self.dim_order == 'pytorch':
+            offset = self.env.observation_space.shape[0]
             axis = 0
             cropped_framebuffer = self.framebuffer[:-offset]
-            img = img.transpose([2,0,1])
         self.framebuffer = np.concatenate([img, cropped_framebuffer], axis = axis)
