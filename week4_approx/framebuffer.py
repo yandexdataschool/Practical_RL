49b9feecda459c8d262e3c56be59d1b08c1bb506
justheuristic <justheuristic@gmail.com>
2018-03-16 20:36:12 +0300

    Add files via upload

---
+++ week4_approx/framebuffer.py
@@ -0,0 +1,41 @@
+import numpy as np
+from gym.spaces.box import Box
+from gym.core import Wrapper
+class FrameBuffer(Wrapper):
+    def __init__(self, env, n_frames=4, dim_order='tensorflow'):
+        """A gym wrapper that reshapes, crops and scales image into the desired shapes"""
+        super(FrameBuffer, self).__init__(env)
+        self.dim_order = dim_order
+        if dim_order == 'tensorflow':
+            height, width, n_channels = env.observation_space.shape
+            obs_shape = [height, width, n_channels * n_frames]
+        elif dim_order == 'pytorch':
+            n_channels, height, width = env.observation_space.shape
+            obs_shape = [n_channels * n_frames, height, width]
+        else:
+            raise ValueError('dim_order should be "tensorflow" or "pytorch", got {}'.format(dim_order))
+        self.observation_space = Box(0.0, 1.0, obs_shape)
+        self.framebuffer = np.zeros(obs_shape, 'float32')
+        
+    def reset(self):
+        """resets breakout, returns initial frames"""
+        self.framebuffer = np.zeros_like(self.framebuffer)
+        self.update_buffer(self.env.reset())
+        return self.framebuffer
+    
+    def step(self, action):
+        """plays breakout for 1 step, returns frame buffer"""
+        new_img, reward, done, info = self.env.step(action)
+        self.update_buffer(new_img)
+        return self.framebuffer, reward, done, info
+    
+    def update_buffer(self, img):
+        if self.dim_order == 'tensorflow':
+            offset = self.env.observation_space.shape[-1]
+            axis = -1
+            cropped_framebuffer = self.framebuffer[:,:,:-offset]
+        elif self.dim_order == 'pytorch':
+            offset = self.env.observation_space.shape[0]
+            axis = 0
+            cropped_framebuffer = self.framebuffer[:-offset]
+        self.framebuffer = np.concatenate([img, cropped_framebuffer], axis = axis)
