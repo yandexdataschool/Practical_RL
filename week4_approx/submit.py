951dfda71928fc089e848a8e7493b4915ce0cd40
Kolesnikov Sergey <sergey.s.kolesnikov@phystech.edu>
2018-03-17 18:20:45 +0300

    w4 updated graders

--- week4_approx/submit.py
+++ week4_approx/submit.py
@@ -4,8 +4,17 @@ sys.path.append("..")
 import grading
 
 
-def submit_sessions(sessions, email, token):
-    session_rewards, session_loss, session_steps = map(np.array, zip(*sessions))
+def submit_cartpole(generate_session, email, token):
+    sessions = [generate_session() for _ in range(100)]
+    session_rewards, _, _ = map(np.array, zip(*sessions))
     grader = grading.Grader("RDofv-QXEeeaGw6kpIOf3g")
     grader.set_answer("NRNkl", int(np.mean(session_rewards)))
+    grader.submit(email, token)
+
+
+def submit_breakout(agent, env, evaluate, email, token):
+    sessions = [evaluate(env, agent, n_games=1) for _ in range(100)]
+    session_rewards = np.array(sessions)
+    grader = grading.Grader("WTOZHCn1EeiNwAoZNi-Hrg")
+    grader.set_answer("VFM7Z", int(np.mean(session_rewards)))
     grader.submit(email, token)
