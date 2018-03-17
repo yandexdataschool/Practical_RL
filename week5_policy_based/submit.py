42c5203c6be0a4fae63edad3694357ac43f935e8
Kolesnikov Sergey <sergey.s.kolesnikov@phystech.edu>
2018-03-17 18:54:52 +0300

    w5 graders

---
+++ week5_policy_based/submit.py
@@ -0,0 +1,20 @@
+import sys
+import numpy as np
+sys.path.append("..")
+import grading
+
+
+def submit_cartpole(generate_session, email, token):
+    sessions = [generate_session() for _ in range(100)]
+    session_rewards = np.array(sessions)
+    grader = grading.Grader("oyT3Bt7yEeeQvhJmhysb5g")
+    grader.set_answer("7QKmA", int(np.mean(session_rewards)))
+    grader.submit(email, token)
+
+
+def submit_kungfu(agent, env, evaluate, email, token):
+    sessions = [evaluate(env, agent, n_games=1) for _ in range(100)]
+    session_rewards = np.array(sessions)
+    grader = grading.Grader("6sPnVCn6EeieSRL7rCBNJA")
+    grader.set_answer("HhNVX", int(np.mean(session_rewards)))
+    grader.submit(email, token)
