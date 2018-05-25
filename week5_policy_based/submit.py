ff739132e7f82461dbd38c0e58ab09be839810d7
Sergey Kolesnikov <sergey.s.kolesnikov@phystech.edu>
2018-05-25 08:07:34 +0300

    Update submit.py
    typo hotfix

--- week5_policy_based/submit.py
+++ week5_policy_based/submit.py
@@ -13,7 +13,7 @@ def submit_cartpole(generate_session, email, token):
 
 
 def submit_kungfu(agent, env, evaluate, email, token):
-    sessions = [evaluate(env, agent, n_games=1) for _ in range(100)]
+    sessions = [evaluate(agent=agent, env=env, n_games=1) for _ in range(100)]
     session_rewards = np.array(sessions)
     grader = grading.Grader("6sPnVCn6EeieSRL7rCBNJA")
     grader.set_answer("HhNVX", int(np.mean(session_rewards)))
