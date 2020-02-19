60b9a5f9a17bf667189d50031ab71f4e24cca351
Michael Diskin <yhn112@users.noreply.github.com>
2020-02-19 12:13:28 +0300

    Update setup_colab.sh
    Fixed bug with outdated repos

--- setup_colab.sh
+++ setup_colab.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # a setup script for google colab. Will be updated
 pip install gym
+apt-get update
 apt-get install -y xvfb
 wget https://raw.githubusercontent.com/yandexdataschool/Practical_DL/fall18/xvfb -O ../xvfb
 apt-get install -y python-opengl ffmpeg
