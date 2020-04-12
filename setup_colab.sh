a18ace493488776bf142c35c7a19bc05441207db
Dmitry Nikulin <1315874+dniku@users.noreply.github.com>
2020-04-12 23:08:09 +0300

    Update setup_colab.sh (#358)
    * Update setup_colab.sh
    
    Gym, PyOpenGL, and FFmpeg are already installed, so we do not need to
    install them explicitly. Also, updated the link to the xvfb script.
    
    * chmod +x and apt-get update

--- setup_colab.sh
+++ setup_colab.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
-# a setup script for google colab. Will be updated
-pip install gym
+#!/usr/bin/env bash
+
 apt-get update
 apt-get install -y xvfb
-wget https://raw.githubusercontent.com/yandexdataschool/Practical_DL/fall18/xvfb -O ../xvfb
-apt-get install -y python-opengl ffmpeg
-pip install pyglet==1.2.4
-
+wget -q https://raw.githubusercontent.com/yandexdataschool/Practical_RL/spring20/xvfb -O ../xvfb
