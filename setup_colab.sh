586c0e2b4599d859227fbb2863ea7dfe275a4926
Lionel Miller <delonorm@gmail.com>
2020-04-16 10:39:21 +0300

    Silence apt-get in Colab

--- setup_colab.sh
+++ setup_colab.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y xvfb
+apt-get -qq update
+apt-get -qq install -y xvfb
 wget -q https://raw.githubusercontent.com/yandexdataschool/Practical_RL/spring20/xvfb -O ../xvfb
