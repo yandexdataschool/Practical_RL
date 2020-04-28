0f87b29c11b906a2affb5f87695595e12a056824
Michael Diskin <yhn112@users.noreply.github.com>
2020-04-29 00:13:29 +0300

    Coursera PyTorch: primer + Week 4 + Week 5 REINFORCE + Week 6 Seq2seq (#341)
    * Approximate q-learning via PyTorch, finally
    
    * pytorch_recap
    
    * DQN via PyTorch
    
    * Add utils.py
    
    * Add atari_wrappers.py
    
    * Fix threshold for dqn_pytorch
    
    * Style fix for approximate q-learning
    
    * Autoformat JSON
    
    * practice_reinforce_pytorch added
    
    * Update submit.py
    
    * Make reinforce work again
    
    * tiny fixes for reinforce
    
    * the tiniest last fix to reinforce
    
    * Port fixes from spring20
    
    * Simplify Colab init in Q-learning & REINFORCE notebooks
    
    * A bit of fixes and prettifying for recap
    
    * Fix colab init in recap
    
    * Seq2seq via pytorch added
    
    * seq2seq minor fixes
    
    * fix colab preamble
    
    Co-authored-by: Lionel Miller <delonorm@gmail.com>

---
+++ week4_approx/utils.py
@@ -0,0 +1,90 @@
+import numpy as np
+import psutil
+from scipy.signal import convolve, gaussian
+import torch
+from torch import nn
+import os
+
+
+def get_cum_discounted_rewards(rewards, gamma):
+    """
+    evaluates cumulative discounted rewards:
+    r_t + gamma * r_{t+1} + gamma^2 * r_{t_2} + ...
+    """
+    cum_rewards = []
+    cum_rewards.append(rewards[-1])
+    for r in reversed(rewards[:-1]):
+        cum_rewards.insert(0, r + gamma * cum_rewards[0])
+    return cum_rewards
+
+
+def play_and_log_episode(env, agent, gamma=0.99, t_max=10000):
+    """
+    always greedy
+    """
+    states = []
+    v_mc = []
+    v_agent = []
+    q_spreads = []
+    td_errors = []
+    rewards = []
+
+    s = env.reset()
+    for step in range(t_max):
+        states.append(s)
+        qvalues = agent.get_qvalues([s])
+        max_q_value, min_q_value = np.max(qvalues), np.min(qvalues)
+        v_agent.append(max_q_value)
+        q_spreads.append(max_q_value - min_q_value)
+        if step > 0:
+            td_errors.append(
+                np.abs(rewards[-1] + gamma * v_agent[-1] - v_agent[-2]))
+
+        action = qvalues.argmax(axis=-1)[0]
+
+        s, r, done, _ = env.step(action)
+        rewards.append(r)
+        if done:
+            break
+    td_errors.append(np.abs(rewards[-1] + gamma * v_agent[-1] - v_agent[-2]))
+
+    v_mc = get_cum_discounted_rewards(rewards, gamma)
+
+    return_pack = {
+        'states': np.array(states),
+        'v_mc': np.array(v_mc),
+        'v_agent': np.array(v_agent),
+        'q_spreads': np.array(q_spreads),
+        'td_errors': np.array(td_errors),
+        'rewards': np.array(rewards),
+        'episode_finished': np.array(done)
+    }
+
+    return return_pack
+
+
+def img_by_obs(obs, state_dim):
+    """
+    Unwraps obs by channels.
+    observation is of shape [c, h=w, w=h]
+    """
+    return obs.reshape([-1, state_dim[2]])
+
+
+def is_enough_ram(min_available_gb=0.1):
+    mem = psutil.virtual_memory()
+    return mem.available >= min_available_gb * (1024 ** 3)
+
+
+def linear_decay(init_val, final_val, cur_step, total_steps):
+    if cur_step >= total_steps:
+        return final_val
+    return (init_val * (total_steps - cur_step) +
+            final_val * cur_step) / total_steps
+
+
+def smoothen(values):
+    kernel = gaussian(100, std=100)
+    # kernel = np.concatenate([np.arange(100), np.arange(99, -1, -1)])
+    kernel = kernel / np.sum(kernel)
+    return convolve(values, kernel, 'valid')
