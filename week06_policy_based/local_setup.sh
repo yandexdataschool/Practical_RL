9fbabb730d4248e74e18f8a586ed934573c01739
justheuristic <justheuristic@gmail.com>
2020-08-04 03:37:55 +0300

    tf==2.0 => tf >= 2.0

--- week06_policy_based/local_setup.sh
+++ week06_policy_based/local_setup.sh
@@ -6,4 +6,4 @@ apt-get install -yqq python-opengl
 python3 -m pip install --user gym==0.14.0
 python3 -m pip install --user pygame
 python3 -m pip install --user pyglet==1.3.2
-python3 -m pip install --user tensorflow==2.0.0
+python3 -m pip install --user tensorflow>=2.0.0
