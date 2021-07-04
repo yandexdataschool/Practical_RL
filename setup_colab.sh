a349a62442fcb0af0f1fbaa052b8b6fe83021485
Lionel Miller <delonorm@gmail.com>
2021-07-04 18:06:14 +0300

    Add Atari ROM import to Colab setup script

--- setup_colab.sh
+++ setup_colab.sh
@@ -3,3 +3,15 @@
 apt-get -qq update
 apt-get -qq install -y xvfb
 wget -q https://raw.githubusercontent.com/yandexdataschool/Practical_RL/master/xvfb -O ../xvfb
+
+# Download & import Atari ROMs (Colab stopped bundling them around the beginning of June 2021)
+
+gdown -q https://drive.google.com/uc?id=1dCLEJcJGDDV4l5ssoexP2TEOVuBfyh7D
+
+# Alternative download:
+# wget -q http://www.atarimania.com/roms/Roms.rar
+
+pip install -q unrar
+mkdir ./roms
+unrar x Roms.rar ./roms > /dev/null 2>&1
+python -m atari_py.import_roms ./roms > /dev/null 2>&1
