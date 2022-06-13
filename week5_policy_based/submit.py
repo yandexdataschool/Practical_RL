8a9ceb908228cb64e51d19f65cdb03328f2d2a0b
ashaba1in <ashabalin9999@gmail.com>
2022-06-13 20:59:24 +0300

    remade tests for week5

--- week5_policy_based/submit.py
+++ week5_policy_based/submit.py
@@ -4,26 +4,22 @@ import tqdm
 
 import sys
 sys.path.append("..")
-import grading
 
 
-def submit_cartpole(generate_session, email, token):
+def test_cartpole(generate_session):
     with gym.make("CartPole-v0").env as env:
         session_rewards = [np.sum(generate_session(env)[2]) for _ in range(100)]
     mean_reward = np.mean(session_rewards)
     print('Your average reward is {} over 100 episodes'.format(mean_reward))
 
-    grader = grading.Grader("oyT3Bt7yEeeQvhJmhysb5g")
-    grader.set_answer("7QKmA", mean_reward)
-    grader.submit(email, token)
+    return mean_reward
 
 
-def submit_kungfu(agent, env, evaluate, email, token):
+def test_kungfu(agent, env, evaluate):
     with tqdm.trange(10, desc='Evaluating your agent') as progress_bar:
         session_rewards = [evaluate(agent=agent, env=env, n_games=1) for _ in progress_bar]
     mean_reward = np.mean(session_rewards)
     print('Your average reward is {} over 10 episodes'.format(mean_reward))
 
-    grader = grading.Grader("6sPnVCn6EeieSRL7rCBNJA")
-    grader.set_answer("HhNVX", mean_reward)
-    grader.submit(email, token)
+    return mean_reward
+
