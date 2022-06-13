8f07875a8f66c9dda86e715c0e671cd8730c4c01
ashaba1in <ashabalin9999@gmail.com>
2022-06-13 20:46:13 +0300

    remade tests for week4

--- week4_approx/submit.py
+++ week4_approx/submit.py
@@ -2,25 +2,20 @@ import sys
 import numpy as np
 import gym
 sys.path.append("..")
-import grading
 
 
-def submit_cartpole(generate_session, email, token):
+def test_cartpole(generate_session):
     with gym.make("CartPole-v0").env as env:
         session_rewards = [generate_session(env) for _ in range(100)]
     mean_reward = np.mean(session_rewards)
     print('Your average reward is {} over 100 episodes'.format(mean_reward))
 
-    grader = grading.Grader("RDofv-QXEeeaGw6kpIOf3g")
-    grader.set_answer("NRNkl", mean_reward)
-    grader.submit(email, token)
+    return mean_reward
 
 
-def submit_breakout(agent, env, evaluate, email, token):
+def test_breakout(agent, env, evaluate):
     session_rewards = [evaluate(env, agent, n_games=1) for _ in range(100)]
     mean_reward = np.mean(session_rewards)
     print('Your average reward is {} over 100 episodes'.format(mean_reward))
 
-    grader = grading.Grader("WTOZHCn1EeiNwAoZNi-Hrg")
-    grader.set_answer("VFM7Z", mean_reward)
-    grader.submit(email, token)
+    return mean_reward
